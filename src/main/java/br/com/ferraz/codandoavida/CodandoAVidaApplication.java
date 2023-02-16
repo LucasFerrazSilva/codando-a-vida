d306054aaddb9b5fc0a2b6c66d27d8cd862de3a6
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2023-02-16 15:51:54 -0300

    Add Spring Boot base files

---
+++ src/main/java/br/com/ferraz/codandoavida/CodandoAVidaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferraz.codandoavida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodandoAVidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodandoAVidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ferraz/codandoavida/CodandoAVidaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferraz.codandoavida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodandoAVidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
